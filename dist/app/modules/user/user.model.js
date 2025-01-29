06b55bdf40dcfb3d1168970721cbb3bb160fbb1f
rahat-495 <md8702687@gmail.com>
2025-01-29 10:28:17 +0600

    users model are created success fully done at dist folder !

--- dist/app/modules/user/user.model.js
+++ dist/app/modules/user/user.model.js
@@ -1,1 +1,31 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.usersModel = void 0;
+const mongoose_1 = require("mongoose");
+const userSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["admin", "user"],
+    },
+    isBlocked: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    timestamps: true,
+});
+exports.usersModel = (0, mongoose_1.model)('User', userSchema);
