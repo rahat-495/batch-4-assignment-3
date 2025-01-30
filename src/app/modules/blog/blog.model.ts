70c32a931ab3b0c9b613d34f0396406eb34bea9a
rahat-495 <md8702687@gmail.com>
2025-01-30 11:18:51 +0600

    blogs modela are created !

---
+++ src/app/modules/blog/blog.model.ts
@@ -0,0 +1,27 @@
+
+import { model, Schema } from "mongoose";
+import { TBlogs } from "./blog.interfaces";
+
+const blogSchema = new Schema<TBlogs>({
+    title : {
+        type : String ,
+        required : true ,
+    },
+    content : {
+        type : String ,
+        required : true ,
+    },
+    author : {
+        type : Schema.Types.ObjectId ,
+        ref : "User" ,
+        required : true ,
+    },
+    isPublished : {
+        type : Boolean ,
+        default : false ,
+    },
+},{
+    timestamps : true ,
+})
+
+export const blogsModel = model<TBlogs>('blog', blogSchema) ;
