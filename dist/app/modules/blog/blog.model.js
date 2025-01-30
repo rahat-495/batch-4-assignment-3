ab21b44332f98dfdd5792b2d36244777f4b0ac96
rahat-495 <md8702687@gmail.com>
2025-01-30 11:19:04 +0600

    blogs modela are created at dist folder !

--- dist/app/modules/blog/blog.model.js
+++ dist/app/modules/blog/blog.model.js
@@ -1,1 +1,26 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.blogsModel = void 0;
+const mongoose_1 = require("mongoose");
+const blogSchema = new mongoose_1.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose_1.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    isPublished: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    timestamps: true,
+});
+exports.blogsModel = (0, mongoose_1.model)('blog', blogSchema);
