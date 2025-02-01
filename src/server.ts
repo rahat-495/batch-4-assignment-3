bbd64fccb384ca88b03a6b8b4968a74f23b6e463
rahat-495 <md8702687@gmail.com>
2025-02-01 22:11:41 +0600

    some error are now handled !

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,37 @@
 
 import mongoose from "mongoose";
-import app from "./app";
+import app from "./app"
 import config from "./app/config";
+import {Server} from "http";
 
+let server : Server ;
 const main = async () => {
+
     try {
 
         await mongoose.connect(config.databaseUrl as string) ;
-
-        app.listen(config.port , () => {
-            console.log(`The server is running on port ${config.port} !`) ;
+    
+        server = app.listen(config.port , () => {
+            console.log(`server are running at port ${config.port} !`) ;
         })
-    }catch(error) {
-        console.log(error) ;   
+        
+    } catch (error) {
+        console.log(error)   
     }
+
 }
 
 main() ;
+
+process.on("unhandledRejection" , () => {
+    if(server){
+        server.close(() => {
+            process.exit(1) ;
+        })
+    }
+    process.exit(1) ;   
+})
+
+process.on("uncaughtException" , () => {
+    process.exit(1) ;   
+})
